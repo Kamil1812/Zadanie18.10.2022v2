57a6b4e4b0225f55bfda00c6b86e87ad8f3ea395
kstajbnz <k.stajkowski@o2.pl>
2022-10-18 10:29:33 +0200

    Initial commit

---
+++ app/src/main/java/com/taboret/zadanie18102022/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.taboret.zadanie18102022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val name = "Kamil Niewiadomski"
+        val textView = findViewById<TextView>(R.id.etykieta)
+        val editText = findViewById<TextInputEditText>(R.id.TextInputEditText)
+
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+            textView.text = textView.text.toString() + editText.text.toString()
+        }
+
+        findViewById<Button>(R.id.btn2).setOnClickListener {
+
+        }
+    }
+}
